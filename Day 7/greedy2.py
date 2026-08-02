22ef7bbad25b2811d4addb636db8fc03ef0d94d5
anu-ship-it <anup17508@gmail.com>
2026-08-02 20:34:02 +0530

    Solved the right question

--- Day 7/greedy2.py
+++ Day 7/greedy2.py
@@ -1,14 +1,16 @@
 t = int(input())
+
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-    ans = 0
-    if a[-1] > 0:
-        ans += 1
+
     for i in range(n - 2, -1, -1):
-        if a[i] <= 0 and a[i + 1] > 0:
+        if a[i + 1] >= 0:
             a[i] += a[i + 1]
-        if a[i] > 0:
+
+    ans = 0
+    for x in a:
+        if x > 0:
             ans += 1
+
     print(ans)
-                     

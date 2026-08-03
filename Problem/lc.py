98acd09523cf18d567c3d189643c67c9f6ecf483
anu-ship-it <anup17508@gmail.com>
2026-08-03 20:40:45 +0530

    Solved another problem

--- Problem/lc.py
+++ Problem/lc.py
@@ -17,3 +17,14 @@ def longestCommonPrefix(self, strs: List[str]) -> str:
     return ans
 print(longestCommonPrefix(None, ["flower", "flow", "flight"]))
 
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+def isPalindrome(self, x: int) -> bool:
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    reversed_half = 0
+    while x > reversed_half:
+        reversed_half = reversed_half * 10 + x % 10
+        x //= 10
+    return x == reversed_half or x == reversed_half // 10
+print(isPalindrome(None, 121))      

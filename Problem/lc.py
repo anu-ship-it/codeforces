dae17317d4b6fa8627d134ce48c5568df158bfdc
anu-ship-it <anup17508@gmail.com>
2026-08-08 12:43:32 +0530

    Solved another problem

--- Problem/lc.py
+++ Problem/lc.py
@@ -145,3 +145,30 @@ print(nums[:k])
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+
+# Given a sorted array of distinct integers and a target value, return
+# the index if the target is found. if not, return the index where it 
+# would be id it inserted in order. 
+
+def searchInsert(self, nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return left
+nums = [3, 2, 2, 3]    
+k = removeElement(nums, 3)
+print(k)
+print(nums[:k])

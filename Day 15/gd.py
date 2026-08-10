77f0bd26b58bde531b88eaadf4815cc1c8a23134
anu-ship-it <anup17508@gmail.com>
2026-08-10 19:29:00 +0530

    Implemeted the right solution

--- Day 15/gd.py
+++ Day 15/gd.py
@@ -1,8 +1,5 @@
 t = int(input())
+
 for _ in range(t):
     n, w = map(int, input().split())
-    if w == 1:
-        print(0)
-    else:    
-        print(n -(n // (w - 1)))
-        
+    print(n - n // w)

3ac3bcb95405ddd11184645c85c14ac519e13629
anu-ship-it <anup17508@gmail.com>
2026-08-12 17:56:12 +0530

    solved another problem

--- Problem/lc.js
+++ Problem/lc.js
@@ -56,3 +56,19 @@ let climbStairs = function(n) {
 };
 console.log(climbStairs(3));
 
+
+// Given the head of a sorted linked list. delete all duplicates such that each element appears
+// only once. Return the linked list sorted as well.
+
+let deleteDuplicates = function(head) {
+    let current = head;
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
+console.log(deleteDuplicates(1,1,2,3,3));

7f942dea89b6e1f5bd00568093f7c26115c9b096
anu-ship-it <anup17508@gmail.com>
2026-08-12 05:56:08 +0530

    Solved an problem

--- Foundation/sum.js
+++ Foundation/sum.js
@@ -1,4 +1,7 @@
 // Write a Program that defines a function to calculate the sum of two integers and prints the result. 
 // Call this function by passing two integer values.
 
-
+function sum(...args){
+    return args.reduce((total, num) => total + num, 0);
+}
+console.log(sum(100, 200, 300, 400));

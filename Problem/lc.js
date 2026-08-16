490ca828f0f57131d7ebdf9c6616b567e4cd1db2
anu-ship-it <anup17508@gmail.com>
2026-08-16 16:03:29 +0530

    Solved another problem

--- Problem/lc.js
+++ Problem/lc.js
@@ -102,3 +102,25 @@ function isSameTree(p, q){
     }
     return true;
 }
+
+
+// Given a binary tree, find its minimum depth.
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+let minDepth = function(root) {
+    if (!root) return 0;
+    const queue = [[root, 1]];
+    while (queue.length > 0) {
+        const [node, depth] = queue.shift();
+        if (!node.left && !node.right) {
+            return depth;
+        }
+        if (node.left) {
+            queue.push([node.left, depth + 1]);
+        }
+        if (node.right) {
+            queue.push([node.right, depth + 1]);
+        }
+    }
+    return 0;
+};

e156e9467d373bd91d717f6dc27dc450d9041b83
anu-ship-it <anup17508@gmail.com>
2026-08-17 18:20:32 +0530

    Solved another problem

--- Problem/lc.js
+++ Problem/lc.js
@@ -137,3 +137,18 @@ let hasPathSum = function(root, targetSum) {
     const remaining = targetSum - root.val;
     return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
 };
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown.
+
+let generate = function(numRows) {
+    const result = [];
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(1 + i).fill(1);
+        for (let j = 1; j < i; j++) {
+            row[j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+        result.push(row);
+    }
+    return result;
+};

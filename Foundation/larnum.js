3554d34fce2f77ece7790410da0d0e85c9fbc1e8
anu-ship-it <anup17508@gmail.com>
2026-08-23 01:19:46 +0530

    Solved the largest number problem

---
+++ Foundation/larnum.js
@@ -0,0 +1,16 @@
+// Write a function secondLargest(arr) that returns the second largest distinct number in an array.
+
+function secondLargest(arr) {
+    if (arr.length < 2) return "Array should have at least two numbers";
+    let first = -Infinity, second = -Infinity;
+    for (let num of arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num !== first) {
+            second = num;
+        }
+    }
+    return second === -Infinity ? "No second largest found" : second;
+}
+console.log(secondLargest([0,3,4,5,6,13,17]));

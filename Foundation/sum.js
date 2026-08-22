76f9f19a368cae1ea40ba3e2a960fa1f39b85134
anu-ship-it <anup17508@gmail.com>
2026-08-23 00:38:43 +0530

    Solved another problem

--- Foundation/sum.js
+++ Foundation/sum.js
@@ -5,3 +5,25 @@ function sum(...args){
     return args.reduce((total, num) => total + num, 0);
 }
 console.log(sum(100, 200, 300, 400));
+
+
+// Given an alphanumeric string s, return the second largest numerical digit that appears in a, or -1 if it does not exist.
+// An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+let secondHighest = function(...s) {
+    let max = -1;
+    let secondMax = -1;
+    for (const c of s) {
+        if (c >= '0' && c <= '9') {
+            const digit = c - '0';
+            if (digit > max) {
+                secondMax = max;
+                max = digit;
+            } else if (digit < max && digit > secondMax) {
+                secondMax = digit;
+            }
+        }
+    }
+    return secondMax;
+};
+console.log(secondHighest(dfa12321afd));

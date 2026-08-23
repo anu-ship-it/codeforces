5c6ada6ab6c09fb0ef28f98763ec36d9ab8bd582
anu-ship-it <anup17508@gmail.com>
2026-08-23 22:27:39 +0530

    Solved another linked list

---
+++ Day 28/lc.js
@@ -0,0 +1,21 @@
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+let hasCycle = function(head) {
+    if (!head || !head.next) return false;   
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;          
+        fast = fast.next.next;     
+
+        if (slow === fast) {       
+            return true;
+        }
+    }
+
+    return false;                  
+};
+console.log(hasCycle([3,2,0,-4]))

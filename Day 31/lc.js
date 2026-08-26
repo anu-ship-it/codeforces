293ebf5096c56cdf0e7a23b850a83de62fc86982
anu-ship-it <anup17508@gmail.com>
2026-08-26 21:22:43 +0530

    Solved another problem

---
+++ Day 31/lc.js
@@ -0,0 +1,13 @@
+let majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+};

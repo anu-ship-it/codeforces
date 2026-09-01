3852deba839d9bbf5b7c566f9e881e80355d96e9
anu-ship-it <anup17508@gmail.com>
2026-09-01 15:26:47 +0530

    Removed the comments

--- Day 35/ary.js
+++ Day 35/ary.js
@@ -8,24 +8,20 @@ let search = function(nums, target) {
         if (nums[mid] === target) {
             return mid;
         }
-
-        // Left half is sorted
         if (nums[left] <= nums[mid]) {
             if (nums[left] <= target && target < nums[mid]) {
-                right = mid - 1;   // target is in the sorted left half
+                right = mid - 1;   
             } else {
-                left = mid + 1;    // target is in the right half
+                left = mid + 1;   
             }
         }
-        // Right half is sorted
         else {
             if (nums[mid] < target && target <= nums[right]) {
-                left = mid + 1;    // target is in the sorted right half
+                left = mid + 1;   
             } else {
-                right = mid - 1;   // target is in the left half
+                right = mid - 1;   
             }
         }
     }
-
     return -1;
 };

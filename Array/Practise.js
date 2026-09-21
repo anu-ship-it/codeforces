815ef392f04315457b4f9101e66bc5a51060a94a
anu-ship-it <anup17508@gmail.com>
2026-09-21 21:31:00 +0530

    Solved few more problem

--- Array/Practise.js
+++ Array/Practise.js
@@ -1,5 +1,13 @@
 // What does the concat() method do in Javascript..?
-ar1 = [1,2,3,4];
+ar1 = [1,9,3,4];
 ar2 = [11, 12, 13, 14];
 let result = ar1.concat(ar2);
 console.log(result);
+
+// Merge teo sorted array
+ar1.sort();
+ar2.sort();
+let array = ar1.concat(ar2);
+console.log(array);
+
+// Remove the Duplicate from an array

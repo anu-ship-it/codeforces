05d411ddd5cf788fbee8c0463db73cd124b621af
anu-ship-it <anup17508@gmail.com>
2026-09-23 01:12:04 +0530

    Solved Few Problems

--- Array/Practise.js
+++ Array/Practise.js
@@ -35,15 +35,15 @@ console.log(count);
 // Find the sum of two numbers in an array and then search them in array which of them would be the sum of those 2 numbers.
 a = [2,7,11,15]
 t = 26
-function TwoSum(arr, target){
-    for(let i = 0; i < a.length; i++) {
-        for(let j = i + 1; j < a.length; j++) {
-            if (a[i]+a[j] === target){
-                return[i,j];
+function TwoSum(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j];
             }
         }
     }
-    return[];
+    return [];
 }
 console.log(TwoSum([2,7,11,15],26));
 
@@ -81,3 +81,50 @@ function removeDuplicate(Array) {
     return Array.slice(0, j + 1);
 }
 console.log(removeDuplicate([1,1,2,4,3,2]));
+
+// Remove Element In-Place(Remove all accurance of 3 in-place)
+function removeElement(arr1, val) {
+    let k = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== val) {
+            arr1[k] = arr1[i];
+            k++;
+        }
+    }
+    return k;
+}
+let arr1 = [3,2,2,3];
+let k = removeElement(arr1, 3);
+console.log(k);
+console.log(arr1.slice(0, k));
+
+
+// Given a sorted array and a target, return the index where the target exists. If it doesn't exist, return the index where it should be inserted to keep the array sorted.(Search Insert Position)
+
+function searchInsert(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= target) {
+            return i;
+        }
+    }
+    return arr.length;
+}
+console.log(searchInsert([1,3,5,6], 6));
+
+// Find the contiguous subarray whose sum is maximum.
+function maxSubArray(arr) {
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (currentSum + arr[i] > arr[i]) {
+            currentSum = currentSum + arr[i];
+        } else {
+            currentSum = arr[i];
+        }
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+    }
+    return maxSum;
+}
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
